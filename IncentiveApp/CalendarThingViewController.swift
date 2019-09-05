f369827447a74bf56c465b87878f891e751f2a3a
SahityaRaipura <43685621+SahityaRaipura@users.noreply.github.com>
2019-09-04 21:51:55 -0400

    Update CalendarThingViewController.swift

--- IncentiveApp/CalendarThingViewController.swift
+++ IncentiveApp/CalendarThingViewController.swift
@@ -2,9 +2,7 @@
 //  CalendarThingViewController.swift
 //  IncentiveApp
 //
-//  Created by Eilat Tabak on 2/10/19.
-//  Copyright © 2019 Eilat Tabak. All rights reserved.
-//
+
 
 import Foundation
 
